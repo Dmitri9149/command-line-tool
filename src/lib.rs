8027cd0d1b5fb1e7bd340d1f53562a27e51acfd3
dmitri9149 <dmitri.as.name@gmail.com>
2020-07-11 13:51:01 +0300

    impl Config is changed

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
@@ -32,7 +33,13 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
